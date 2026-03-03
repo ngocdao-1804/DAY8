897fc377513d15bcbec61de8f2567b2bf6e8f1a7
ngocdao-1804 <accloneofdao@gmail.com>
2026-03-03 20:22:35 +0700

    ok

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-// /bai1
+/**Bai 2*/
+const intergers=[1,2,3,4,6,7,8,9,10,11];
+const evenNumbers = intergers.filter(
+    (intergers, index)=> {
+        return intergers%2===0;
+    }
+);
+console.log(evenNumbers);
+//FILTER
+// const numbers = [1,2,3,4, 9, 10];
+
+// const filteredNumbers = numbers.filter(
+//     (number, index) => {
+//         return number<=5
+//     }
+// )
+//  /bai1
 // / method ~ hàm
 // forEach
-const provinces = [`Quang Ngai`,`Binh Phuoc`,`Quang Nam`,`Quang Ninh`,`Ninh Binh`, `Bac Ninh`]
-provinces.forEach((provinces, index)=>{
-    console.log(`${provinces} - ${index}`);
-})
+// const provinces = [`Quang Ngai`,`Binh Phuoc`,`Quang Nam`,`Quang Ninh`,`Ninh Binh`, `Bac Ninh`]
+// provinces.forEach((provinces, index)=>{
+//     console.log(`${provinces} - ${index}`);
+// })
