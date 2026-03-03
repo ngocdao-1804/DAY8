b47dd1671dd561b2c4951581ca987503bdf1e92e
ngocdao-1804 <accloneofdao@gmail.com>
2026-03-03 20:11:44 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// /bai1
+// / method ~ hàm
+// forEach
+const provinces = [`Quang Ngai`,`Binh Phuoc`,`Quang Nam`,`Quang Ninh`,`Ninh Binh`, `Bac Ninh`]
+provinces.forEach((provinces, index)=>{
+    console.log(`${provinces} - ${index}`);
+})
